c7b49aa9ec7b6237449ec4a703406e77ec859822
kiznn <luis.mp.cunha@enta.pt>
2024-09-24 20:12:56 +0000

    IT WORKS; I THINK

--- lottery.py
+++ lottery.py
@@ -2,6 +2,7 @@ from random import randint as r
 
 
 class Lottery:
+
     def __init__(self):
         pass
 
@@ -31,3 +32,22 @@ class Lottery:
 
         return nums, stars
 
+    def sort_nums(self, nums):
+
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i + 1, len(nums)):
+                if nums[min_index] > nums[j]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+
+        return nums
+
+    def sort_stars(self, stars):
+
+        if stars[1] < stars[0]:
+            stars[0], stars[1] = stars[1], stars[0]
+        else:
+            pass
+
+        return stars
--- main.py
+++ main.py
@@ -5,27 +5,28 @@ lottery = Lottery()
 # Main program
 while True:
    try:
-       quantas = int(input('Quantas? '))
+       quantidade = int(input('Quantas? '))
        break
    except ValueError:
        print('Digite um número')
 
 nums = []
 starss = []
+results = []
+quantas = quantidade
 
 while quantas > 0:
     # Generate lottery numbers and stars
     numeros, stars = lottery.create_arrays()
 
     if numeros not in nums and stars not in starss:
-        nums.append(numeros)
-        starss.append(stars)
-
-        results = [nums, starss]
+        lottery.sort_nums(numeros)
+        lottery.sort_stars(stars)
+        results = [numeros] + [stars]
 
         quantas -= 1
 
-        print(f'Todos os resultados até agora: {results}')
+        print(f'Todos os resultado numero {quantidade-quantas} até agora: {results}')
     else:
         print(f'{numeros} ou {stars} são duplicados, gerando novamente...')
 
